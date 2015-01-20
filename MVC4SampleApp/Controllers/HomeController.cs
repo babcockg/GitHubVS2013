2ff6117b06816d8ad191dd25e08da07b71ceb464
babcockg <galen.babcock@gmail.com>
2015-01-20 11:28:01 -0600

    Commments

---
+++ MVC4SampleApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace MVC4SampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Index
+        /// </summary>
+        /// <returns>View</returns>
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
